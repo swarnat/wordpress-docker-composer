6c847345f34a5ac2c5673dda6f1979c988faa3ac
Stefan Warnat <ich@stefanwarnat.de>
2024-01-14 18:14:42 +0100

    Update startup.sh
    Execute wpd-script when composer is installed

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,6 +5,8 @@ cd /var/www/
 echo "Install composer"
 composer install -d /var/www/
 
+composer run-script wpd-script
+
 php /scripts/startup.php
 
 cp /var/www/crons/cron-wp.dist.yaml /var/www/crons/cronjobs.yaml
